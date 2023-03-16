90733ca01f88db13309eb29505275a25412c17b6
TERAL2 <teral2@MacBook-Air-de-TERAL2.local>
2023-03-16 17:58:51 -0500

    zoom scale add zoom

--- Pages/Components/IosZoomImage.tsx
+++ Pages/Components/IosZoomImage.tsx
@@ -27,28 +27,36 @@ const IosZoomImage = ({ ImageUrl }: inputProps) => {
                     const distance = Math.sqrt(dx * dx + dy * dy);
                     const initalScale = Math.abs(distance / scaleMovement);
                     const currentScale = scale._value;
-                    const newScale = currentScale + initalScale - 1;
-                    scale._value < 1 ?
+                    const newScale = Math.abs(currentScale + initalScale - 1);
+                    if (scale._value < 1 && newScale < 1 && initalScale < 1 && scale._value < 2 && newScale <2 && initalScale < 2){
                         scale.setValue(1)
-                        : scale._value > 20 ?
-                            scale.setValue(20)
-                            : scale.setValue(newScale)
+                        console.log('scaleOne', scale)
+                        // pan.setValue({x: 0, y:0});
+                        // focalPoint.setValue({x:0, y:0})
+                        console.log('pan scale 1', pan, 'focalpoint scale 1', focalPoint)
+                    }else if(scale._value > 20 && newScale > 20){
+                        scale.setValue(20)
+                        console.log('scaletwenty', scale)
+                    }else if (newScale > 1 && newScale < 20){
+                        scale.setValue(newScale)
+                        console.log('new scale', scale)
+                    }
                     const centerPoint = { // different focal point to pinch to zoom
-                        x: -((event.nativeEvent.touches[0].pageX + event.nativeEvent.touches[1].pageX - (width)) / 5),
-                        y: -((event.nativeEvent.touches[0].pageY + event.nativeEvent.touches[1].pageY - (height)) / 5),
+                        x: -((event.nativeEvent.touches[0].pageX + event.nativeEvent.touches[1].pageX - (width)) / 2),
+                        y: -((event.nativeEvent.touches[0].pageY + event.nativeEvent.touches[1].pageY - (height)) / 2),
                     };
                     const newFocalPoint = {
                         x: centerPoint.x,
                         y: centerPoint.y
                     };
                     focalPoint.setValue(newFocalPoint)
-                    console.log('focal point', focalPoint)
                 } else if (scale._value > 1 && touches.length === 1) { //Pan gesture
                     Animated.event([null, { dx: pan.x, dy: pan.y }], { useNativeDriver: false })(event, { ...gesture, dx: gesture.dx / scale._value, dy: gesture.dy / scale._value });
                 }
             },
             onPanResponderRelease: () => {
                 pan.extractOffset();
+                //scale.extractOffset();
             },
         }),
     ).current;
@@ -86,15 +94,15 @@ const IosZoomImage = ({ ImageUrl }: inputProps) => {
                             { translateX: focalPoint.x },
                             { translateY: focalPoint.y },
                             { scale: scale },
-                            { translateX: focalPoint.x },
-                            { translateY: focalPoint.y },
+                            // { translateX: focalPoint.x },
+                            // { translateY: focalPoint.y },
                         ]
                     },
                     ]}
                     {...panResponder.panHandlers}
-                // onLoad={(e) => {
-                //     setSizeImage({x:e.nativeEvent.source.width, y: e.nativeEvent.source.height });
-                // }}
+                onLoad={(e) => {
+                    setSizeImage({x:e.nativeEvent.source.width, y: e.nativeEvent.source.height });
+                }}
                 // onLayout={({ nativeEvent }) =>
                 // focalPoint.setValue({
                 // x: nativeEvent.layout.width / 2,
